f9e947d5a7cf9a0c7df27d6b74deea91b90ec3f5
Krishna Daga <krishnadaga100@gmail.com>
2026-01-03 06:08:27 +0530

    Initial project structure

---
+++ fraud-detection-engine/src/test/java/com/inhuman/frauddetectionengine/FraudDetectionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inhuman.frauddetectionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
